adb97c2f126040333a061501a11b35d6ad840b2f
Connor_Murray <connormurray215@gmail.com>
2019-07-17 23:04:41 -0400

    ready

--- routes/user.js
+++ routes/user.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router  = express.Router();
 const userController = require('../controllers/user')
-const isAuthenticated = require('../config/middleware/isAuthenticated');
+// const isAuthenticated = require('../config/middleware/isAuthenticated');
 
-router.get( '/user/signup', userController.register );
+// router.get( '/user/signup', userController.register );
 
-router.get( '/user/login', userController.userLogin )
+// router.get( '/user/login', userController.userLogin )
 
-router.post( '/user/signup', userController.signup );
+// router.post( '/user/signup', userController.signup );
 
-router.post('/user/login', userController.userSignin );
+// router.post('/user/login', userController.userSignin );
 
-router.get('/profile', isAuthenticated, userController.userProfile);
+// router.get('/profile', isAuthenticated, userController.userProfile);
 
-router.get('/logout', userController.userLogout);
+// router.get('/logout', userController.userLogout);
 
 module.exports = router;
 
