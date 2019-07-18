b7db84101a03e9f79ebe4420cd8d85d699c6861c
Connor_Murray <connormurray215@gmail.com>
2019-07-18 02:58:35 -0400

    Ready for pulling

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const express = require('express');
 const routes  = require('./routes');
 const session = require('express-session');
 const bodyParser = require('body-parser').urlencoded({extended: false})
+// const database = require('./models');
+
 
 const app = express();
+
+// const port = process.env.PORT || 3000 ;
 
 app.set("view engine","ejs");
 
@@ -13,5 +17,13 @@ app.use(express.static('./public'));
 app.use(bodyParser)
 app.use(routes);
 
+
+// database.sequelize.sync().then(function(){
+//     app.listen(port, function(err){
+//         if (err)
+//             console.log(err)
+//         console.log('Server is live on port: ' + port)
+//     })
+// });
 app.listen(3000)
-console.log('we are live on port 3000')
+console.log('live on port 3000')
