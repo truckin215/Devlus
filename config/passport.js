1d8b23a28f8f3e915621a23b37e0ea64b06d189b
Connor_Murray <connormurray215@gmail.com>
2019-07-23 14:29:08 -0400

    making progress in passport

--- config/passport.js
+++ config/passport.js
@@ -4,15 +4,14 @@ const db = require('../models');
 var routes = require('../routes/index');
 
 // telling passport we want to use local strategy, in other words we want to use
-// email and password
+// Username and password
 // for signing in
 passport.use(new LocalStrategy(
-    {  usernameField: 'email' },
-    function(email, password, done) {
-      db.User.findOne( { where: { email: email } } ).then( function(dbuser){
+    function(usernameField, password, done) {
+      db.User.findOne( { where: { usernameField: usernameField } } ).then( function(dbuser){
         // if there is no user with the provided email
         if (!dbuser) {
-            return done(null, false, { message: 'Incorrect Email.' } );
+            return done(null, false, { message: 'Incorrect Username.' } );
         }
         // if there is a user with that email but the password is wrong
         else if (!dbuser.validPassword(password)) {
@@ -29,8 +28,8 @@ passport.use(new LocalStrategy(
 passport.use('local-signup', new LocalStrategy(
     {  usernameField: 'email',
        passReqToCallback: true }, // allow us to use the entire request coming from our app},
-    function(req, email, password, done) {
-      db.User.findOne( { where: { email: email } } ).then( function(dbuser){
+    function(req, username, password, done) {
+      db.User.findOne( { where: { username: username } } ).then( function(dbuser){
         // to verify that the email is not in use
         if (dbuser) {
             return done(null, false, { message: 'Email is already taken.' } );
